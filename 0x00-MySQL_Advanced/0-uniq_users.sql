ed235ccea45d75b4c8222a0edea5768338605ac7
abenezerayalew <aben.man.unity@gmail.com>
2023-07-13 23:05:33 +0300

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS uniq_users;
+-- create table users with unique users
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
